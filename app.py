629c88e699aa4c23e50c41e594e52f68c47782fd
s-kuehn <amazingsdk@gmail.com>
2019-12-12 10:28:52 -0500

    Completed the db functionality

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/', methods=['GET','POST'])
 def home():
-
-
+	# create table if not already existing
+	conn = sqlite3.connect('calculations.db')
+	c = conn.cursor()
+	c.execute("""CREATE TABLE IF NOT EXISTS joblist (
+				id INTEGER PRIMARY KEY,
+				project_name TEXT,
+				description TEXT,
+				due_date INTEGER,
+				shoot_date INTEGER,
+				purpose TEXT,
+				budget INTEGER,
+				special_requests TEXT)""")
+	conn.commit()
+	conn.close()
 
 	if request.form:
-		# Assign variables to form values
+		# Assign form values to variables
 		project_name = request.form['project_name']
 		description = request.form['description']
 		due_date = request.form['due_date']
@@ -18,28 +31,32 @@ def home():
 		budget = request.form['budget']
 		special_requests = request.form['special_requests']
 
-		# Add row to table and create table if not already existing
+		# Add row to sql table
 		conn = sqlite3.connect('calculations.db')
 		c = conn.cursor()
-
-		c.execute("""CREATE TABLE IF NOT EXISTS joblist (
-					id INTEGER PRIMARY KEY,
-					project_name TEXT,
-					description TEXT,
-					due_date INTEGER,
-					shoot_date INTEGER,
-					purpose TEXT,
-					budget INTEGER,
-					special_requests TEXT)""")
-		c.execute("INSERT INTO joblist(project_name, description, due_date, shoot_date, purpose, budget, special_requests) VALUES("+"'"+project_name+"'"+", "+description+", "+due_date+", "+shoot_date+", "+special_requests+")")
-		c.execute("SELECT * FROM joblist")
-		items = c.fetchall()
-		for item in items:
-			print(item)
+		c.execute("INSERT INTO joblist(project_name, description, due_date, shoot_date, purpose, budget, special_requests) VALUES("+"'"+project_name+"'"+", "+"'"+description+"'"+", "+"'"+due_date+"'"+", "+"'"+shoot_date+"'"+", "+"'"+purpose+"'"+", "+"'"+budget+"'"+", "+"'"+special_requests+"'"+")")
 		conn.commit()
 		conn.close()
-	return render_template('index.html', items=items)
 
+		# load submitted route
+		return redirect('/submited')
+
+	# Print contents of sql table
+	conn = sqlite3.connect('calculations.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM joblist")
+	items = c.fetchall()
+	for item in items:
+		print(item)
+	conn.commit()
+	conn.close()
+
+	# Display index.html
+	return render_template('index.html')
+
+# Submitted route
 @app.route('/submited')
 def submitted():
-	pass
+
+	# Display submitted.html
+	return render_template('submitted.html')
